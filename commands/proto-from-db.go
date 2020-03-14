f2022f559e58652f9021f8d3dbbaffd4ac2083d3
zokypesch <maulanaoktofitriadi@gmail.com>
2020-03-14 22:33:47 +0700

    remove utils

--- commands/proto-from-db.go
+++ commands/proto-from-db.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 	"unicode"
 
+	core "github.com/zokypesch/proto-lib/core"
 	utils "github.com/zokypesch/proto-lib/utils"
 	tmp "github.com/zokypesch/proto-tools/template"
 )
@@ -102,7 +103,7 @@ func (cmd *ProtoFromDB) Execute(args map[string]string) error {
 	var tables []string
 	var err error
 
-	db := utils.InitDBWithoutLog(host, dbName, user, pass, 3306)
+	db := core.InitDBWithoutLog(host, dbName, user, pass, 3306)
 	err = db.Raw("SHOW TABLES").Pluck(fmt.Sprintf("Tables_in_%s", dbName), &tables).Error
 
 	if err != nil {
